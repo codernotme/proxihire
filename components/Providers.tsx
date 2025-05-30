a5f1e0c4330806361f7ff39e64b5cc5bdb07b451
Aryan <coolaryanbajpai20@gmail.com>
2025-05-30 21:39:42 +0530

    added heroui

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { HeroUIProvider } from "@heroui/react";
+
+
 export default function Providers({
   children,
 }: Readonly<{
@@ -8,7 +11,9 @@ export default function Providers({
   return (
     <html lang="en">
       <body>
+        <HeroUIProvider>
         {children}
+        </HeroUIProvider>
       </body>
     </html>
   );
